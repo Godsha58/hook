d468be6499f39785de296d24dea7d27a19f1e7f2
Godsha58 <jroberto.rore04@gmail.com>
2024-06-24 12:53:49 -0600

    sigo si saber usar git (odio git)

---
+++ src/DataDisplay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import useFetchData from './hooks/useFetchData';
+
+const DataDisplay = () => {
+  const { data, loading, error } = useFetchData('https://jsonplaceholder.typicode.com/posts');
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <h1>Fetched Data:</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataDisplay;
